c17dfd125338e164baa944870e3c6df2cfa8d1bd
Çagdas Bozman <cagdas.bozman@ocamlpro.com>
2013-05-16 14:25:09 +0200

    Add example for clone and prependTo methods

--- examples/test.ml
+++ examples/test.ml
@@ -8,8 +8,11 @@ let d = D.document
 let jss = Js.string
 let _ = (jQ "div.foo")##css (jss "color", jss "red")
 
-let _ = 
+let _ =
   (jQ "#clickme")##click (some (wrap_callback (fun _ ->
-    (jQ "#square")##slideDown (some (of_duration Slow), null, some (wrap_callback (fun _ -> 
-      Dom_html.window##alert(Js.string "Done sliding")))))))
+    (jQ "#square")##slideDown (some (of_duration Slow), null, some (wrap_callback (fun _ ->
+      D.window##alert(jss "Done sliding")))))))
+
+let _ =
+  ((jQ "b")##clone(Js._false, Js._false))##prependTo(jss "p")
 
